3fd14b21cc43e72651680f90a2ef3d8b70fb88f4
YgorTrocoli <ygoraraujoca@outlook.com>
2022-03-21 13:44:35 -0300

    Last commit

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,11 +1,80 @@
+const User = require('../models/User')
+
+const bcrypt = require('bcryptjs')
+
+
 module.exports = class AuthController {
 
     static login(req, res) {
         res.render('auth/login')
     }
+    static async loginPost(req, res) {
+        const {email, password} = req.body
+        const user = await User.findOne({where: {email:email}})
+        if(!user){
+            req.flash('message','Email not found')
+            res.render('auth/login')
+            return
+        }
+        const passwordMatch = bcrypt.compareSync(password, user.password)
+        if(!passwordMatch) {
+            req.flash('message','Invalid password!')
+            res.render('auth/login')
+            return
+        }
 
+        // if both confirmations works initialize session
+        req.session.userid = user.id
+        req.flash('message','Logged in!')
+        req.session.save(() => {
+            res.redirect('/')
+        })
+    }
     static register(req, res) {
         res.render('auth/register')
     }
+    static async registerPost(req, res) {
 
+        const {name, email, password, confirmpassword} = req.body
+        // passwrod confirmation validation
+        if(password != confirmpassword) {
+            // mensagem
+            req.flash('message', 'Passwords does not match!')
+            res.render('auth/register')
+            return
+        }
+        // check if user exists
+        const checkIfUserExists = await User.findOne({where: {email: email}})
+        if(checkIfUserExists) {
+            req.flash('message', 'this Email is already in use')
+            res.render('auth/register')
+            return
+        }
+        // create a password
+        const salt = bcrypt.genSaltSync(10)
+        const hashedPassword = bcrypt.hashSync(password, salt)
+        const user = {
+            name,
+            email,
+            password: hashedPassword
+        }
+        try {
+            const createdUser = await User.create(user)
+
+            // initialize session after register
+            req.session.userid = createdUser.id 
+
+            req.flash('message', 'Created successfully!')
+            req.session.save(() => {
+                res.redirect('/')
+            })
+
+        } catch (error) {
+            console.log(err)
+        }
+    }
+    static logout(req, res) {
+        req.session.destroy()
+        res.redirect('/login')
+    }
 }
--- controllers/ThoughtController.js
+++ controllers/ThoughtController.js
@@ -1,8 +1,133 @@
 const Thought = require('../models/Thought')
 const User = require('../models/User')
 
+const { Op } = require('sequelize')
+
 module.exports = class ThoughtController {
+
+
     static async showThoughts(req, res) {
-        res.render('thoughts/home')
+
+        let search = ''
+        if(req.query.search) {
+            search = req.query.search
+        }
+
+        let order = 'DESC'
+        if(req.query.order === 'old') {
+            order = 'ASC'
+        }else {
+            order = 'DESC'
+        }
+
+        const thoughtsData = await Thought.findAll({
+            include: User,
+            where: {
+                title: { [Op.like]: `%${search}%`},
+            },
+            order: [['createdAt', order]]
+
+        })
+        // map to receive just the data of the object
+        const thoughts = thoughtsData.map((result) => result.get({plain: true}))
+
+        let thoughtsQty = thoughts.length
+        if(thoughtsQty === 0) {
+            thoughtsQty = false
+        }
+        res.render('thoughts/home', { thoughts, search, thoughtsQty })
+    }
+
+
+    static async dashboard(req, res) {
+        const userId = req.session.userid
+
+        const user = await User.findOne({
+            where: {
+                id: userId,
+            },
+            include: Thought,
+            plain: true,
+        })
+        if(!user) { //check if user exists or is logged in
+            res.redirect('/login')
+        }
+
+        const thoughts = user.Thoughts.map((result) => result.dataValues)
+
+        // check if dashboard is empty to send message
+        let emptyThoughts = false
+        if(thoughts.length === 0) {
+            emptyThoughts = true
+        }
+
+        res.render('thoughts/dashboard', { thoughts, emptyThoughts })
+    }
+
+
+    static createThought(req, res) {
+        res.render('thoughts/create')
+    }
+
+
+    static async createThoughtSave (req, res) {
+        const thought = {
+            title: req.body.title,
+            UserId: req.session.userid 
+        }
+
+        try {
+            await Thought.create(thought)
+            req.flash('message', 'Your thoughts were heard!')
+    
+            req.session.save(() => {
+                res.redirect('/thoughts/dashboard')
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+    static async removeThought(req, res) {
+        const id = req.body.id
+        const UserId = req.session.userid
+        try {
+            await Thought.destroy({where: {id: id, UserId: UserId}})
+            req.flash('message', 'Your thought was deleted!')
+            req.session.save(() => {
+                res.redirect('/thoughts/dashboard')
+            })
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+    static async updateThought(req, res) {
+        const id = req.params.id
+        const thought = await Thought.findOne({where: {id: id}, raw: true})
+        res.render('thoughts/edit', { thought })
+    }
+
+
+    static async updateThoughtSave (req, res) {
+        
+        const id = req.body.id
+        const thought = {
+
+            title: req.body.title,
+        }
+        try {
+            await Thought.update(thought, {where: {id: id}})
+            req.flash('message', 'Successfully edited!')
+            req.session.save(() => {
+                res.redirect('/thoughts/dashboard')
+            })
+        } catch (error) {
+            console.log(error)
+        }
+
     }
 }
